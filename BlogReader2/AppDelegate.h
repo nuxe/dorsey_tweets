270bba1b76a8ff932650c45f525860b0c3bc204a
Class Account <cs198-ow@akane.cs.berkeley.edu>
2013-03-30 18:07:02 -0700

    Initial Commit

---
+++ BlogReader2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReader2
+//
+//  Created by Class Account on 3/30/13.
+//  Copyright (c) 2013 Kush Agrawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReader2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader2
+//
+//  Created by Class Account on 3/30/13.
+//  Copyright (c) 2013 Kush Agrawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
