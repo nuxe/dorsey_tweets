5dcccfada5af11757d5da66682eeb302fab730ae
Class Account <cs198-ow@akane.cs.berkeley.edu>
2013-03-30 20:59:55 -0700

    Parses JSON data and displays it

---
+++ TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  BlogReader2
+//
+//  Created by Class Account on 3/30/13.
+//  Copyright (c) 2013 Kush Agrawal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
